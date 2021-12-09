7ecf8993d7e0c330795fe0ed83c7989ffb44a15b
Andrea Julca Batch <andrea.julca@gmail.com>
2021-12-08 19:22:38 -0500

    oh nice it works on my machine

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
 //We know this demo from https://codesandbox.io/s/react-xr-hands-demo-gczkp?file=/src/index.tsx works;
+import { OrbitControls } from '@react-three/drei/core/OrbitControls'
+import { Sky } from '@react-three/drei/core/Sky'
 import ReactDOM from 'react-dom'
 import React from 'react'
 import { VRCanvas, Hands, DefaultXRControllers } from '@react-three/xr'
 import './App.css';
-import HandText from './Components/Somatic/MageHand';
+import Pinchable from './Components/Somatic/MageHand';
 
-
-// was trying
-// <HandText />
-// after <Hands/>, but it keeps getting caught in a suspense loop.
-
-// Not sure why that one broke, but https://codesandbox.io/s/47vqp?file=/src/App.tsx works.
+// Not sure why other joint pos demo breaks, but https://codesandbox.io/s/47vqp?file=/src/App.tsx works.
 function App() {
   return (
-    <div className="App">
     <VRCanvas>
-      <ambientLight />
-      <pointLight position={[10, 10, 10]} />
-      <DefaultXRControllers />
+      <Pinchable />
       <Hands />
+      <OrbitControls />
+      <ambientLight />
+      <pointLight position={[1, 1, 1]} />
+      <color args={['black']} attach="background" />
+      {/* <Sky sunPosition={[500, 500, 500]} /> */}
     </VRCanvas>
-    </div>
   );
 }
 
--- src/Components/Somatic/MageHand.tsx
+++ src/Components/Somatic/MageHand.tsx
@@ -1,44 +1,117 @@
-import * as THREE from 'three'
-import React, { Suspense, useState, useRef, useLayoutEffect, useMemo } from 'react';
-import { useThree, useFrame } from '@react-three/fiber';
-import { FontLoader } from 'three/examples/jsm/loaders/FontLoader'
-import { useLoader } from '@react-three/fiber'
-import { Mesh } from 'three';
+import * as THREE from 'three';
+import React, { useEffect, useRef, useState } from 'react';
+import { RoundedBox, Sphere, Text } from '@react-three/drei';
+import { extend, reconciler, useFrame, useThree } from '@react-three/fiber';
+import { BufferGeometry, CatmullRomCurve3, Line, Material, Mesh, MeshBasicMaterial, MeshPhongMaterial, SplineCurve, Vector3 } from 'three';
+import { useSpring, config } from 'react-spring';
 
-function HandText(props: any) {
-    const { gl } = useThree()
-    const [finished, setFinished] = useState(false)
-    const [handdata, setHanddata] = useState(''); 
-    const font = useLoader(FontLoader, '/bold.blob')
-    const config = useMemo(() => ({ font, size: 40, height: 50 }), [font])
-    const mesh = useRef<Mesh>(null)
-    useLayoutEffect(() => {
-        const size = new THREE.Vector3()
-        mesh.current?.geometry.computeBoundingBox();
-        mesh.current?.geometry?.boundingBox?.getSize(size);
-        mesh.current?.position?.set(-size.x / 2, -size.y / 2, -1);
-    }, [handdata])
+const start = new Vector3(-0.25, 1.0, -0.3)
+const end = new Vector3(0.25, 1.0, -0.3)
+const startEnd = new Vector3().copy(end).sub(start)
 
-    useFrame((state) => {
-        if (!finished){
-            const joint = (gl.xr as any).getHand(0).joints['index-finger-tip'];
-            if (joint?.jointRadius !== undefined){
-                setHanddata(joint.toString());
-                setFinished(true);
-            } 
-        }
-    })
+function getTargetPos(handle: Vector3) {
+  return new Vector3().copy(handle).sub(start).projectOnVector(startEnd).add(start)
+}
 
-    return (
-        <Suspense fallback={null}>
-        <group {...props} scale={[1, 1, 0.1]}>
-          <mesh ref={mesh}>
-            <textGeometry args={[{handdata}, config]} />
-            <meshNormalMaterial />
-          </mesh>
-        </group>
-        </Suspense>
-    )
-  }
-  
-export default HandText;
+const curve = new CatmullRomCurve3([start, new Vector3().lerpVectors(start, end, 0.5), end], false, 'catmullrom', 0.25)
+
+const offset = new Vector3()
+
+let prevPinching = false
+
+let velocity = new Vector3(0, 0, 0)
+let targetPos = getTargetPos(new Vector3(0, 0, 0))
+
+let frame = 0
+
+function Pinchable({ children }: any) {
+  const { gl } = useThree()
+  const hand = gl.xr.getHand(1) as any
+  const ref = useRef<Mesh | null>(null)
+  const v = new Vector3()
+  const [label, setLabel] = useState(0)
+
+  const [props, set] = useSpring(() => ({ scale: 1, config: config.wobbly }))
+
+  useEffect(() => {
+    if (!ref.current ) return
+  }, [])
+
+  useFrame(() => {
+    if (!ref.current) return
+
+    const handle = ref.current as any
+    const index = hand.joints['index-finger-tip']
+    const thumb = hand.joints['thumb-tip']
+
+    let pinching = false
+
+    frame++
+
+    if (index && thumb) {
+      const pinch = Math.max(0, 1 - index.position.distanceTo(thumb.position) / 0.1)
+      pinching = pinch > 0.82
+      const pointer = v.lerpVectors(index.position, thumb.position, 0.5)
+      const distance = pointer.distanceTo(handle.position)
+
+      const tDistance = 0.2
+      if (distance < tDistance) {
+        let scale = 1 + (0.8 - pinch) * 0.4 * (1 - distance / tDistance)
+        handle.scale.set(scale, scale, scale)
+      }
+
+      handle.material.color.set(pinching ? 'hotpink' : 'white');
+        
+      // Save offset when starting to ping
+      if (pinching && !prevPinching) {
+        offset.copy(pointer).sub(handle.position)
+      }
+
+      // Move pointer to the hand
+      if (pinching) {
+        handle.position.copy(pointer).sub(offset)
+        curve.points[1].copy(handle.position)
+      }
+    }
+
+    // Released
+    if (prevPinching && !pinching) {
+      velocity.set(0, 0, 0)
+      targetPos.copy(getTargetPos(handle.position))
+    }
+
+    // return to the position
+    if (!pinching) {
+      const direction = new Vector3().copy(targetPos).sub(handle.position)
+      velocity.add(direction.multiplyScalar(0.05))
+      velocity.multiplyScalar(0.92)
+      handle.position.add(velocity)
+    }
+
+    // Update line
+    curve.points[1].copy(handle.position)
+
+    // get current position
+    let value = Math.floor((getTargetPos(handle.position).sub(start).length() / startEnd.length()) * 100)
+    if (frame % 5 === 0) {
+      setLabel(value)
+    }
+
+    prevPinching = pinching
+  })
+
+  return (
+    <>
+      <Sphere args={[0.01]} position={start} />
+      <Sphere args={[0.01]} position={end} />
+      <RoundedBox args={[0.03, 0.08, 0.08]} radius={0.01} ref={ref} position={[0, 1.2, -0.3]}>
+        <meshStandardMaterial />
+        <Text fontSize={0.02} position={[0, 0.07, 0]}>
+          {label}
+        </Text>
+      </RoundedBox>
+    </>
+  )
+}
+
+export default Pinchable;

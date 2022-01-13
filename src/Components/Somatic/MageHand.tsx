bdcb2fc64045b37144c7ab5f7c3caf1c22e2a463
Andrea Julca Batch <andrea.julca@gmail.com>
2022-01-13 16:21:26 -0500

    in theory this should drag a sphere along the path traveled by each fingertip; in practice it does not

--- src/Components/Somatic/MageHand.tsx
+++ src/Components/Somatic/MageHand.tsx
@@ -2,11 +2,12 @@
  * Instead of the box interactions, we want to trigger this when the user
  * is trying to record an event and log their gestures as a command.
  * Also, remove the box now that we've confirmed it works with our config.
+ * See https://immersive-web.github.io/webxr-hand-input/#skeleton-joints for joint refs
  */
 
 import * as THREE from 'three';
 import React, { useEffect, useRef, useState } from 'react';
-import { RoundedBox, Sphere, Text } from '@react-three/drei';
+import { RoundedBox, Sphere, Text, CurveModifier } from '@react-three/drei';
 import { extend, reconciler, useFrame, useThree } from '@react-three/fiber';
 import { BufferGeometry, CatmullRomCurve3, Line, Material, Mesh, MeshBasicMaterial, MeshPhongMaterial, SplineCurve, Vector3 } from 'three';
 import { useSpring, config } from 'react-spring';
@@ -31,6 +32,73 @@ let targetPos = getTargetPos(new Vector3(0, 0, 0))
 let frame = 0
 
 function Pinchable({ children }: any) {
+  const jointNames = [
+    "wrist",
+  
+    "thumb-metacarpal",
+    "thumb-phalanx-proximal",
+    "thumb-phalanx-distal",
+    "thumb-tip",
+  
+    "index-finger-metacarpal",
+    "index-finger-phalanx-proximal",
+    "index-finger-phalanx-intermediate",
+    "index-finger-phalanx-distal",
+    "index-finger-tip",
+  
+    "middle-finger-metacarpal",
+    "middle-finger-phalanx-proximal",
+    "middle-finger-phalanx-intermediate",
+    "middle-finger-phalanx-distal",
+    "middle-finger-tip",
+  
+    "ring-finger-metacarpal",
+    "ring-finger-phalanx-proximal",
+    "ring-finger-phalanx-intermediate",
+    "ring-finger-phalanx-distal",
+    "ring-finger-tip",
+  
+    "pinky-finger-metacarpal",
+    "pinky-finger-phalanx-proximal",
+    "pinky-finger-phalanx-intermediate",
+    "pinky-finger-phalanx-distal",
+    "pinky-finger-tip"
+  ];
+
+  const justTheTips = [
+    "thumb-tip",
+    "index-finger-tip",
+    "middle-finger-tip",
+    "ring-finger-tip",
+    "pinky-finger-tip"
+  ]
+  
+  //may as well do it this way
+  /*const tipCurves = {
+    "thumb-tip": new CatmullRomCurve3([start, new Vector3().lerpVectors(start, end, 0.5), end], false, 'catmullrom', 0.25),
+    "index-finger-tip": new CatmullRomCurve3([start, new Vector3().lerpVectors(start, end, 0.5), end], false, 'catmullrom', 0.25),
+    "middle-finger-tip": new CatmullRomCurve3([start, new Vector3().lerpVectors(start, end, 0.5), end], false, 'catmullrom', 0.25),
+    "ring-finger-tip": new CatmullRomCurve3([start, new Vector3().lerpVectors(start, end, 0.5), end], false, 'catmullrom', 0.25),
+    "pinky-finger-tip": new CatmullRomCurve3([start, new Vector3().lerpVectors(start, end, 0.5), end], false, 'catmullrom', 0.25)
+  }*/
+
+  const fingstart = new THREE.Vector3(0, 0, 0)
+  const [curveThum, setCurveThum] = useState(new CatmullRomCurve3([fingstart, fingstart], false, 'catmullrom', 0.25));
+  const [curveIndx, setCurveIndx] = useState(new CatmullRomCurve3([fingstart, fingstart], false, 'catmullrom', 0.25));
+  const [curveMidl, setCurveMidl] = useState(new CatmullRomCurve3([fingstart, fingstart], false, 'catmullrom', 0.25));
+  const [curveRing, setCurveRing] = useState(new CatmullRomCurve3([fingstart, fingstart], false, 'catmullrom', 0.25));
+  const [curvePink, setCurvePink] = useState(new CatmullRomCurve3([fingstart, fingstart], false, 'catmullrom', 0.25));
+  
+  const curveRefThum = useRef()
+  const curveRefIndx = useRef()
+  const curveRefMidl = useRef()
+  const curveRefRing = useRef()
+  const curveRefPink = useRef()
+  
+
+
+  const curve = new CatmullRomCurve3([start, new Vector3().lerpVectors(start, end, 0.5), end], false, 'catmullrom', 0.25)
+  
   const { gl } = useThree()
   const hand = gl.xr.getHand(1) as any
   const ref = useRef<Mesh | null>(null)
@@ -47,12 +115,44 @@ function Pinchable({ children }: any) {
     if (!ref.current) return
 
     const handle = ref.current as any
-    const index = hand.joints['index-finger-tip']
     const thumb = hand.joints['thumb-tip']
+    const index = hand.joints['index-finger-tip']
+    
+    //actually need to do it this way
+    const middle = hand.joints['middle-tip']
+    const ring = hand.joints['ring-tip']
+    const pinky = hand.joints['pinky-tip']
 
     let pinching = false
-
+    
     frame++
+
+    //have to do it by individual finger
+    if(thumb){
+      setCurveThum(new CatmullRomCurve3([...curveThum.points, thumb.position], false, 'catmullrom', 0.25))
+    }
+    if(index){
+      setCurveIndx(new CatmullRomCurve3([...curveIndx.points, index.position], false, 'catmullrom', 0.25))
+    }
+    if(middle){
+      setCurveMidl(new CatmullRomCurve3([...curveMidl.points, middle.position], false, 'catmullrom', 0.25))
+    }
+    if(ring){
+      setCurveRing(new CatmullRomCurve3([...curveRing.points, ring.position], false, 'catmullrom', 0.25))
+    }
+    if(pinky){
+      setCurvePink(new CatmullRomCurve3([...curvePink.points, pinky.position], false, 'catmullrom', 0.25))
+    }
+
+
+    /*
+    for(let jointName in justTheTips){
+      const thisJoint = hand.joints[jointName];
+      if (thisJoint){
+        tipCurves[jointName].points.append(thisJoint.position);
+      }
+    }
+    */
 
     if (index && thumb) {
       const pinch = Math.max(0, 1 - index.position.distanceTo(thumb.position) / 0.1)
@@ -116,6 +216,22 @@ function Pinchable({ children }: any) {
           {label}
         </Text>
       </RoundedBox>
+      <CurveModifier ref={curveRefThum} curve={curveThum}>
+        <mesh><Sphere args={[0.01]} position={start} /></mesh>
+      </CurveModifier>
+      <CurveModifier ref={curveRefIndx} curve={curveIndx}>
+        <mesh><Sphere args={[0.01]} position={start} /></mesh>
+      </CurveModifier>
+      <CurveModifier ref={curveRefMidl} curve={curveMidl}>
+        <mesh><Sphere args={[0.01]} position={start} /></mesh>
+      </CurveModifier>
+      <CurveModifier ref={curveRefRing} curve={curveRing}>
+        <mesh><Sphere args={[0.01]} position={start} /></mesh>
+      </CurveModifier>
+      <CurveModifier ref={curveRefPink} curve={curvePink}>
+        <mesh><Sphere args={[0.01]} position={start} /></mesh>
+      </CurveModifier>
+
     </>
   )
 }

bd926f84b345219df12cda8eee3ac54e52dd9253
Andrea Julca Batch <andrea.julca@gmail.com>
2022-01-13 14:43:23 -0500

    Now at least it CLAIMS to be using the mic

--- src/Components/Verbal/SpeechToText.js
+++ src/Components/Verbal/SpeechToText.js
@@ -2,6 +2,26 @@ import React from 'react';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 
 const Dictaphone = () => {
+  //versions without react extension
+  /*
+  let SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition
+  let SpeechGrammarList = window.SpeechGrammarList || window.webkitSpeechGrammarList
+  
+  SpeechRecognition.onresult = function(event) {
+    for(var word in event.results[0].transcript){
+      console.log('Word: ' + word);
+    }
+  }
+
+  return (
+    <div>
+      <button onClick={SpeechRecognition.start}>Start</button>
+      <button onClick={SpeechRecognition.stop}>Stop</button>
+    </div>
+  );
+
+  */
+
   const {
     transcript,
     listening,
@@ -13,13 +33,17 @@ const Dictaphone = () => {
     return <span>Browser doesn't support speech recognition.</span>;
   }
 
+  const listenContinuously = () => SpeechRecognition.startListening({
+    continuous: true
+  })
+
   return (
     <div>
       <p>Microphone: {listening ? 'on' : 'off'}</p>
-      <button onClick={SpeechRecognition.startListening}>Start</button>
+      <button onClick={listenContinuously}>Start</button>
       <button onClick={SpeechRecognition.stopListening}>Stop</button>
       <button onClick={resetTranscript}>Reset</button>
-      <p>{transcript}</p>
+      <p>{transcript}</p>      
     </div>
   );
 };
